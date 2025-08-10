87e1fcea1774fd2c33d0c837f3d9c9433e93a410
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-08-10 15:52:13 -0500

    rename some builtins, add is_int

--- ci-lisp/src/env/math.rs
+++ ci-lisp/src/env/math.rs
@@ -12,6 +12,17 @@ pub fn math_environment(env: Environment) -> Environment {
         }
     ));
 
+    #[allow(unreachable_patterns)]
+    let env = env.insert("is_int", native_fn!(
+        (a), {
+            let res = matches!(a, AstNode::Value(Value::Int(_)));
+            match res {
+                true => Ok(AstNode::Value(Value::True)),
+                false => Ok(AstNode::Value(Value::Nil))
+            }
+        }
+    ));
+
     let env = env.insert("eq", native_fn!(
         (AstNode::Value(a), AstNode::Value(b)), {
             if a == b {
@@ -32,17 +43,17 @@ pub fn math_environment(env: Environment) -> Environment {
         }
     ));
 
-    let env = env.insert("add", native_fn!(
+    let env = env.insert("builtin__int_add", native_fn!(
         (AstNode::Value(Value::Int(a)), AstNode::Value(Value::Int(b))), {
             Ok(AstNode::Value(Value::Int(a + b)))
         }
     ));
-    let env = env.insert("mul", native_fn!(
+    let env = env.insert("builtin__int_mul", native_fn!(
         (AstNode::Value(Value::Int(a)), AstNode::Value(Value::Int(b))), {
             Ok(AstNode::Value(Value::Int(a * b)))
         }
     ));
-    let env = env.insert("sub", native_fn!(
+    let env = env.insert("builtin__int_sub", native_fn!(
         (AstNode::Value(Value::Int(b)), AstNode::Value(Value::Int(a))), {
             Ok(AstNode::Value(Value::Int(a - b)))
         }
