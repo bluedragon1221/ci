2762ee5eb7c102f0da2a7bc200890af8f0a453da
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-08-11 15:31:05 -0500

    start using cargo workspace

--- flake.nix
+++ flake.nix
@@ -3,13 +3,23 @@
   outputs = inputs: let
     system = "x86_64-linux";
     pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+    libPath = pkgs.lib.makeLibraryPath (with pkgs; [
+      libGL
+      libxkbcommon
+      wayland
+    ]);
   in {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
-        rustc
         cargo
+        rustc
         rust-analyzer
       ];
+
+      RUST_LOG = "debug";
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      LD_LIBRARY_PATH = libPath;
     };
   };
 }
