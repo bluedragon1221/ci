1f27d27f2a4cd9784e53b65a4b21d558a6a7719a
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-08-12 18:55:13 -0500

    prepare for non-linux systems (future)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,13 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = inputs: let
-    system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    forAllSystems = function:
+      inputs.nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        # Don't have access to any non-linux systems to test on right now
+      ] (system: function inputs.nixpkgs.legacyPackages.${system});
 
-    commonArgs = {
+    commonArgs = pkgs: {
       RUST_LOG = "debug";
       RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
@@ -15,20 +18,22 @@
       ]);
     };
   in {
-    devShells.${system}.default = pkgs.mkShell (
-      commonArgs
-      // {
-        packages = with pkgs; [
-          cargo
-          rustc
-          rust-analyzer
-        ];
-      }
-    );
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell (
+        (commonArgs pkgs)
+        // {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+          ];
+        }
+      );
+    });
 
-    packages.${system} = {
+    packages = forAllSystems (pkgs: {
       ci-term = pkgs.rustPlatform.buildRustPackage (
-        commonArgs
+        (commonArgs pkgs)
         // {
           pname = "ci-term";
           version = "0.1.0";
@@ -38,15 +43,15 @@
         }
       );
       ci-gui = pkgs.rustPlatform.buildRustPackage (
-        commonArgs
+        (commonArgs pkgs)
         // {
           pname = "ci-gui";
-          versions = "0.1.0";
+          version = "0.1.0";
           src = ./.;
           cargoBuildFlags = "--bin ci-gui";
           cargoLock.lockFile = ./Cargo.lock;
         }
       );
-    };
+    });
   };
 }
