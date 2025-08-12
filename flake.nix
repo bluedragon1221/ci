7ec9eaa7f43970ce59716a8dded639b96290ed5b
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-08-12 18:46:51 -0500

    add packages to flake (using buildRustPackage for now)

--- flake.nix
+++ flake.nix
@@ -1,25 +1,52 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   outputs = inputs: let
     system = "x86_64-linux";
     pkgs = inputs.nixpkgs.legacyPackages.${system};
 
-    libPath = pkgs.lib.makeLibraryPath (with pkgs; [
-      libGL
-      libxkbcommon
-      wayland
-    ]);
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        cargo
-        rustc
-        rust-analyzer
-      ];
-
+    commonArgs = {
       RUST_LOG = "debug";
       RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      LD_LIBRARY_PATH = libPath;
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+        libGL
+        libxkbcommon
+        wayland
+      ]);
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell (
+      commonArgs
+      // {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+        ];
+      }
+    );
+
+    packages.${system} = {
+      ci-term = pkgs.rustPlatform.buildRustPackage (
+        commonArgs
+        // {
+          pname = "ci-term";
+          version = "0.1.0";
+          src = ./.;
+          cargoBuildFlags = "--bin ci-term";
+          cargoLock.lockFile = ./Cargo.lock;
+        }
+      );
+      ci-gui = pkgs.rustPlatform.buildRustPackage (
+        commonArgs
+        // {
+          pname = "ci-gui";
+          versions = "0.1.0";
+          src = ./.;
+          cargoBuildFlags = "--bin ci-gui";
+          cargoLock.lockFile = ./Cargo.lock;
+        }
+      );
     };
   };
 }
